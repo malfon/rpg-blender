c63cfc4842c8e97d1ab1e551db1d475d0c4d0ef5
malfon <cortneyjeans@gmail.com>
2013-10-16 11:02:03 -0400

    Create RPG - Dialogue.java

---
+++ RPG - Dialogue.java
@@ -0,0 +1,24 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Dialogue
+{
+	static Graphics g;
+	
+	public Dialogue() throws SlickException
+	{
+		
+	}
+	
+	public static void render(GameContainer gc, StateBasedGame game, Graphics g)
+	{
+		
+	}
+	
+	public static void update(GameContainer gc, StateBasedGame game)
+	{
+		
+	}
+}
